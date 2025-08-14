e5ef3e1ebf5b87020c0593168f55a56803ff6fa8
Vivekbansal2703 <vivekbansal2005@gmail.com>
2025-08-14 19:07:33 +0530

    fixing favicon name

--- client/app/layout.js
+++ client/app/layout.js
@@ -7,7 +7,7 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Horizone",
+  title: "Horizone.ai",
   description: "Find your Dream Car",
 };
 
